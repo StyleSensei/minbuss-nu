0bb339cd8beabb89458b3ff7d1be1364ab00a63c
Patrik Arell <patrikarell@gmail.com>
2025-05-27 23:56:05 +0200

    refactor: change populateDb endpoint from GET to POST and remove unused cron job configuration

--- src/app/api/cron/populateDb/route.ts
+++ src/app/api/cron/populateDb/route.ts
@@ -2,9 +2,7 @@ import { extractZip } from "@/app/services/dataProcessors/extractZip";
 import { saveToDatabase } from "@/app/services/dataProcessors/saveToDatabase";
 import type { NextRequest } from "next/server";
 
-export const maxDuration = 60;
-
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
 	const authHeader = request.headers.get("authorization");
 	if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
 		return new Response("Unauthorized", {
