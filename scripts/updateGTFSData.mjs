39c5fa5bbfa9ef0680a65790e6d7cc2725a5f105
Patrik Arell <patrikarell@gmail.com>
2025-05-28 11:09:45 +0200

    fix: update cron job to compile TypeScript before running GTFS update script and adjust import paths in updateGTFSData script

--- scripts/updateGTFSData.mjs
+++ scripts/updateGTFSData.mjs
@@ -1,5 +1,5 @@
-import { extractZip } from "../src/app/services/dataProcessors/extractZip.ts";
-import { saveToDatabase } from "../src/app/services/dataProcessors/saveToDatabase.ts";
+import { extractZip } from "../src/app/services/dataProcessors/extractZip.js";
+import { saveToDatabase } from "../src/app/services/dataProcessors/saveToDatabase.js";
 
 async function main() {
 	try {
