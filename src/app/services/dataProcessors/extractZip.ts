256af98f7524dc5a463a7fcc13536ebe8c519fde
Patrik Arell <patrikarell@gmail.com>
2025-05-28 11:34:31 +0200

    fix: update package.json and tsconfig.json for module type and resolution, adjust import paths in extractZip.ts to include .js extension

--- src/app/services/dataProcessors/extractZip.ts
+++ src/app/services/dataProcessors/extractZip.ts
@@ -1,11 +1,11 @@
 import type { Readable } from "node:stream";
-import { getStaticData } from "../dataSources/gtfsStatic";
+import { getStaticData } from "../dataSources/gtfsStatic.js";
 const unzipper = require("unzipper");
 import csvParser from "csv-parser";
-import type { IRoute } from "@/app/models/IRoute";
-import type { ITrip } from "@/app/models/ITrip";
-import type { IStop } from "@/app/models/IStop";
-import type { IStopTime } from "@/app/models/IStopTime";
+import type { IRoute } from "@/app/models/IRoute.js";
+import type { ITrip } from "@/app/models/ITrip.js";
+import type { IStop } from "@/app/models/IStop.js";
+import type { IStopTime } from "@/app/models/IStopTime.js";
 
 export const extractZip = async () => {
 	const routes: IRoute[] = [];
