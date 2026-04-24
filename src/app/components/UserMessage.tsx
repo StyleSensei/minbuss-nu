e9314fbe7962203a62fc28b7b0aa3421682cba0c
Patrik Arell <patrikarell@gmail.com>
2026-04-24 21:45:04 +0200

    refactor: update button and message components for improved hover effects and layout consistency

--- src/app/components/UserMessage.tsx
+++ src/app/components/UserMessage.tsx
@@ -1,28 +1,33 @@
-import { info2 } from "../../../public/icons";
-import { Icon } from "./Icon";
+import { info2 } from '../../../public/icons';
+import { Icon } from './Icon';
 
 type UserMessageProps = {
-	title?: string;
-	message?: string;
+  title?: string;
+  message?: string;
 };
 
 const UserMessage = ({
-	title = "Platstjänster avslaget.",
-	message = "Aktivera platstjänster för full funktionallitet.",
+  title = 'Platstjänster avslaget.',
+  message = 'Aktivera platstjänster för full funktionallitet.',
 }: UserMessageProps) => {
-	return (
-		<>
-			<p className="warn-message">
-				<Icon
-					path={info2.path}
-					iconSize="20px"
-					fill="black"
-					title="info"
-					className="no-position-icon"
-				/>
-				<strong>{title}</strong> {message}
-			</p>
-		</>
-	);
+  return (
+    <>
+      <article className='warn-message'>
+        <div className='warn-message-title'>
+          <Icon
+            path={info2.path}
+            iconSize='20px'
+            fill='black'
+            title='info'
+            className='no-position-icon'
+          />
+          <p>
+            <strong>{title}</strong>
+          </p>
+        </div>
+        <p>{message}</p>
+      </article>
+    </>
+  );
 };
 export default UserMessage;
